4c12fb2a98fab667a2261f6b67a57e91d154e1ba
petrucciii <riccardo.petrucci99@gmail.com>
2021-06-20 18:13:21 +0200

    header

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Sito Ufficiale della pensione per cani: 'La Pensione di Apollo.'">
+    <meta name="description" content="Sito Ufficiale della pensione per cani: 'L'Impero di Apollo.'">
     <meta name="keywords" content="">
 	<link rel="shortcut icon" href="./views/images/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="views/css/style.css">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <title>La Pensione di Apollo</title>
+    <title>L'Impero di Apollo</title>
 </head>
 <body>
     <?php
