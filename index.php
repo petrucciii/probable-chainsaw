a5250bdb5ca4ddf2b2123f2e4c65126e00608273
petrucciii <riccardo.petrucci99@gmail.com>
2021-06-20 11:43:01 +0200

    init

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Sito Ufficiale di">
+    <meta name="keywords" content="">
+    <link rel="stylesheet" href="views/css/style.css">
+    <title></title>
+</head>
+<body>
+    <?php
+        include './views/index.html';
+		
+		
+    ?>
+</body>
+</html>
